f747e93043ee1f0dda8664c848ffa74f43cedf28
Constantin <kostiantynkalinin@gmail.com>
2016-02-09 13:15:10 +0000

    Challenge 10

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,11 @@ class Bookmark < Sinatra::Base
     'Hello Bookmark!'
   end
 
+  get '/links' do
+    @links = Link.all
+    erb :'/links/index'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
